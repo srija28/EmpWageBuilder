fdae9d53bf653adfda9b8719017f833ef692783c
shreya <shreyareddykethireddy@shreyas-MacBook-Air.local>
2020-09-24 14:09:21 +0530

    First Commit

--- EmpWageBuilderUC1.java
+++ EmpWageBuilderUC1.java
@@ -13,21 +13,3 @@ class EmpWageBuilderUC1 {
 
 }
 
-//UC2
-
-class EmpWageBuilderUC2 {
-	public static void main(String[] args) {
-		int IS_FULL_TIME = 1;
-		int EMP_RATE_PER_HOUR = 20;
-		int empHrs = 0;
-		int empWage=0;
-		double empCheck = Math.floor(Math.random()*10)%2;
-		if(empCheck == IS_FULL_TIME)
-			empHrs=8;
-		else
-			empHrs=0;
-		empWage = empHrs * EMP_RATE_PER_HOUR;
-		System.out.println("Emp Wage: "+ empWage);
-	}
-
-}
